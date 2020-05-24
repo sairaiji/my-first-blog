23119f8f13142d8c5d3a4cae37a8e2b3d98f9647
sairaiji <sai.ko.ace@icloud.com>
2020-05-24 10:50:51 +0800

    Made changes on HTML & CSS

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-import os.path
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -120,6 +119,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-#STATIC_ROOT = os.path.join(BASE_DIR,'static')
-STATIC_ROOT = ''
-STATICFILES_DIR = ( os.path.join('static'),)
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
+
